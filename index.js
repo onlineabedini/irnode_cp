8b61a6eab0e8ecdf3490621c8327b2b0f829f436
AlirezaAbedini <onlineabedini@gmail.com>
2022-05-04 22:43:42 +0430

    project added to git

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = new class main_cp {
+    constructor() {
+        this.start_log();
+    }
+    start_log(){
+        console.log(`
+            -- irnode ---------------------------------
+            *******************************************
+            --  project is running on cp
+            --  onlineabedini - 00989367312702
+            -- ----------------------------------------
+                warning ~ project is under development
+            -- ----------------------------------------
+            *******************************************
+          `);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# irnode_cp
